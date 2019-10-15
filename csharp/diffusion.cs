0167222ce077b1186545a445d464067fc438c4f3
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-15 19:29:37 -0400

    Comment syntax added to header

--- csharp/diffusion.cs
+++ csharp/diffusion.cs
@@ -1,6 +1,6 @@
-Programmer: Christian Winds
-Course: CSC 330 Organization of Programming Languages
-School Year: 2019-2020 School Year
+// Programmer: Christian Winds
+// Course: CSC 330 Organization of Programming Languages
+// School Year: 2019-2020 School Year
 
 // Code from Dr. Pounds,
 // /tmp/diffusion.java
