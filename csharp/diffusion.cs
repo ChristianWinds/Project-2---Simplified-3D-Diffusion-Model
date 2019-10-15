d0836bab51ad0cf91ddfc4b983a7a936010bd536
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-15 19:23:33 -0400

    do for loop partially added

---
+++ csharp/diffusion.cs
@@ -0,0 +1,39 @@
+Programmer: Christian Winds
+Course: CSC 330 Organization of Programming Languages
+School Year: 2019-2020 School Year
+
+// Code from Dr. Pounds,
+// /tmp/diffusion.java
+// Accessed October 15th, 2019
+do
+{
+	for (int i = 0; i < maxSize; i++)
+	{
+		for (int j = 0; j < maxSize; j++)
+		{
+			for (int k = 0; k < maxSize; k++)
+			{
+				for (int l = 0; l < maxSize; l++)
+				{
+					for (int m = 0; m < maxSize; m++)
+					{
+						for (int n = 0; n < maxSize; j++)
+						{
+							if (((i == l) && (j == m) && (k == n + 1)) ||
+							    ((i == l) && (j == m) && (k == n - 1)) ||
+							    ((i == l) && (j == m + 1) && (k == n)) ||
+							    ((i == l) && (j == m - 1) && (k == n)) ||
+							    ((i == l + 1) && (j == m) && (k == n)) ||
+							    ((i == l - 1) && (j == m) && (k == n)))
+							{
+								;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+ratio = minval / maxval;
+} while (ratio < 0.99);
