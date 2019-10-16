6a4fb22be09ed7df61c6807a12467c5d175519f3
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-16 10:48:19 -0400

    Class name added

--- csharp/diffusion.cs
+++ csharp/diffusion.cs
@@ -7,81 +7,84 @@
 // Accessed October 15th, 2019
 using System;
 
-static void Main()
+class diffusion
 {
-	readonly int maxSize = 10;
-	double[, ,] cube = new double[maxSize, maxSize, maxSize];
-
-	// Zero the cube.
-	for (int i = 0; i < maxSize; i++)
+	static void Main()
 	{
-		for (int j = 0; j < maxSize; j++)
-		{
-			for (int k = 0; k < maxSize; k++)
-			{
-				cube[i, j, k] = 0.0;
-			}
-		}
-	}
+		readonly int maxSize = 10;
+		double[, ,] cube = new double[maxSize, maxSize, maxSize];
 
-	double diffusionCoefficient = 0.175;
-	double roomDimension = 5;
-	double speedOfGasMolecules = 250.0;
-	double timestep = (roomDimension / speedOfGasMolecules) / maxSize;
-	double distanceBetweenBlocks =  roomDimension / maxSize;
-
-	double DTerm = diffusionCoefficient * timestep / (distanceBetweenBlocks * distanceBetweenBlocks)
-
-	// Initialize the first cell.
-	cube[0, 0, 0] = 1.0e21;
-
-	int pass = 0;
-
-	// Create a double variable to track increasing system time.
-	double time = 0.0;
-
-	double ratio = 0.0;
-
-	do
-	{
+		// Zero the cube.
 		for (int i = 0; i < maxSize; i++)
 		{
 			for (int j = 0; j < maxSize; j++)
 			{
 				for (int k = 0; k < maxSize; k++)
 				{
-					for (int l = 0; l < maxSize; l++)
-					{
-						for (int m = 0; m < maxSize; m++)
-						{
-							for (int n = 0; n < maxSize; j++)
-							{
-								if (((i == l) && (j == m) && (k == n + 1)) ||
-								    ((i == l) && (j == m) && (k == n - 1)) ||
-								    ((i == l) && (j == m + 1) && (k == n)) ||
-								    ((i == l) && (j == m - 1) && (k == n)) ||
-								    ((i == l + 1) && (j == m) && (k == n)) ||
-								    ((i == l - 1) && (j == m) && (k == n)))
-								{
-									double change = (cube[i, j, k] - cube[l, m, n]) * DTerm;
-									cube[i, j, k] = cube[i, j, k] - change;
-									cube[l, m, n] = cube[l, m, n] + change;
-								}
-							}
-						}
-					}
-
-					time = time + timestep;
-					
-					// Check for mass consistency.
-					double sumVal = 0.0;
-					double[, ,] maxVal = new cube[0, 0, 0];
-					double minVal = cube[0, 0, 0];
-
+					cube[i, j, k] = 0.0;
 				}
 			}
 		}
 
-	ratio = minval / maxval;
-	} while (ratio < 0.99);
+		double diffusionCoefficient = 0.175;
+		double roomDimension = 5;
+		double speedOfGasMolecules = 250.0;
+		double timestep = (roomDimension / speedOfGasMolecules) / maxSize;
+		double distanceBetweenBlocks =  roomDimension / maxSize;
+
+		double DTerm = diffusionCoefficient * timestep / (distanceBetweenBlocks * distanceBetweenBlocks)
+
+		// Initialize the first cell.
+		cube[0, 0, 0] = 1.0e21;
+
+		int pass = 0;
+
+		// Create a double variable to track increasing system time.
+		double time = 0.0;
+
+		double ratio = 0.0;
+
+		do
+		{
+			for (int i = 0; i < maxSize; i++)
+			{
+				for (int j = 0; j < maxSize; j++)
+				{
+					for (int k = 0; k < maxSize; k++)
+					{
+						for (int l = 0; l < maxSize; l++)
+						{
+							for (int m = 0; m < maxSize; m++)
+							{
+								for (int n = 0; n < maxSize; j++)
+								{
+									if (((i == l) && (j == m) && (k == n + 1)) ||
+									    ((i == l) && (j == m) && (k == n - 1)) ||
+									    ((i == l) && (j == m + 1) && (k == n)) ||
+									    ((i == l) && (j == m - 1) && (k == n)) ||
+									    ((i == l + 1) && (j == m) && (k == n)) ||
+									    ((i == l - 1) && (j == m) && (k == n)))
+									{
+										double change = (cube[i, j, k] - cube[l, m, n]) * DTerm;
+										cube[i, j, k] = cube[i, j, k] - change;
+										cube[l, m, n] = cube[l, m, n] + change;
+									}
+								}
+							}
+						}
+
+						time = time + timestep;
+					
+						// Check for mass consistency.
+						double sumVal = 0.0;
+						double[, ,] maxVal = new cube[0, 0, 0];
+						double minVal = cube[0, 0, 0];
+
+					}
+				}
+			}
+
+		ratio = minval / maxval;
+		} while (ratio < 0.99);
+	}
 }
