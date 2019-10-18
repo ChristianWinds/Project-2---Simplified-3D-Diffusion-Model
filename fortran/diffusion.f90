fd5a4518c8a55e3305f483c53b561d85ff2b2dc3
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-18 15:32:36 -0400

    Update

--- fortran/diffusion.f90
+++ fortran/diffusion.f90
@@ -8,6 +8,10 @@ integer :: maxSize
 real :: diffusionCoefficient, roomDimension, speedOfGasMolecules, timestep,
 distanceBetweenBlocks, dTerm, time, ratio, change, sumVal, maxVal, minVal
 
+maxSize 10
+cube maxSize maxSize maxSize
+
+! Zero the cube
 print *, time, ' ', ratio, ' ', sumVal
 print *, 'Box equilibrated in ', time, ' seconds of simulated time.'
 end program diffusion
