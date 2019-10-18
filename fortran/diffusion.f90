8fcab8eabd95e8f74272b3c7e9967cbcddbd918d
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-18 15:26:26 -0400

    Variable declaration added

--- fortran/diffusion.f90
+++ fortran/diffusion.f90
@@ -3,7 +3,11 @@
 ! School Year: 2019-2020 School Year
 
 program diffusion
+implicit none
+integer :: maxSize
+real :: diffusionCoefficient, roomDimension, speedOfGasMolecules, timestep,
+distanceBetweenBlocks, dTerm, time, ratio, change, sumVal, maxVal, minVal
 
-print *, time, ' ', ratio, ' ', sumval
+print *, time, ' ', ratio, ' ', sumVal
 print *, 'Box equilibrated in ', time, ' seconds of simulated time.'
 end program diffusion
