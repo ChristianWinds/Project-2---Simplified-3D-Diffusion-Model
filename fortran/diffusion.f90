b80e770913e929cdf51c87f190a32c2d17fce8f1
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-18 15:53:33 -0400

    Updated, header added

--- fortran/diffusion.f90
+++ fortran/diffusion.f90
@@ -2,6 +2,10 @@
 ! Course: CSC 330 Organization of Programming Languages
 ! School Year: 2019-2020 School Year
 
+! Code from Dr. Pounds,
+! /tmp/diffusion.java
+! Accessed October 15th, 2019
+
 program diffusion
 implicit none
 integer :: maxSize
@@ -19,6 +23,22 @@ do  i = 0, maxSize
                 end do
         end do
 end do
+
+diffusionCoefficient = 0.175;
+
+! Create a variable for room dimension to represent 5 meters
+roomDimension = 5;
+
+! Set a gas molecule speed variable to represent gas molecules' speed at room
+! temperature
+speedOfGasMolecules = 250.0;
+
+! Create a timestep variable to represent the value of h in seconds
+timestep = (roomDimension / speedOfGasMolecules) / maxSize;
+distanceBetweenBlocks = roomDimension / maxSize;
+
+DTerm = diffusionCoefficient * timestep / (distanceBetweenBlocks * distanceBetweenBlocks);
+
 print *, time, ' ', ratio, ' ', sumVal
 print *, 'Box equilibrated in ', time, ' seconds of simulated time.'
 end program diffusion
