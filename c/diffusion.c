0dd634a9116b517056f1a5481e51ee4c4b42dde4
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-23 09:36:03 -0400

    Partial do loop imported from C# diffusion.cs

--- c/diffusion.c
+++ c/diffusion.c
@@ -28,8 +28,44 @@ int main()
 	// Initialize the first cell
 	cube[0][0][0] = 1.0e21;
 
+	double time = 0.0;
+
+	double ratio = 0.0;
+
 	do
+	{
+		for (i = 0; i < maxSize; i++)
+		{
+			for (j = 0; j < maxSize; j++)
+			{
+				for (k = 0; k < maxSize; k++)
+				{
+					for (l = 0; l < maxSize; l++)
+					{
+						for (m = 0; m < maxSize; m++)
+						{
+							for (n = 0; n < maxSize; n++)
+							{
+								if (((i == l) && (j == m) && (k == n + 1)) ||
+										((i == l) && (j == m) && (k == n - 1)) ||
+										((i == l) && (j == m + 1) && (k == n)) ||
+										((i == l) && (j == m - 1) && (k == n)) ||
+										((i == l + 1) && (j == m) && (k == n)) ||
+										((i == l - 1) && (j == m) && (k == n)))
+								{
+									double change = (cube[i, j, k] - cube[l, m, n]) * DTerm;
+									cube[i, j, k] = cube[i, j, k] - change;
+									cube[l, m, n] = cube[l, m, n] + change;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		time = time + timestep;
 
 
-	return 0;
-}
+		return 0;
+	}
