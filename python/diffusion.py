eb15356f16c045fe20464af03147fcc07fe7024e
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-29 08:43:59 -0400

    Updated

--- python/diffusion.py
+++ python/diffusion.py
@@ -8,16 +8,31 @@ maxSize = 10
 
 # Zero the cube
 
+diffusionCoefficient = 0.175
+
 # Create a variable for room dimension to represent 5 meters
+roomDimension = 5
 
 # Set a gas molecule speed variable to represent 100 g/mol gas molecules' speed
 # at room temperature
+speedOfGasMolecules = 250.0
 
 # Create a timestep variable to represent the value of h in seconds
+timestep = (roomDimension / speedOfGasMolecules) / maxSize
+
+distanceBetweenBlocks = roomDimension / maxSize
+
+dTerm = diffusionCoefficient * timestep / (distanceBetweenBlocks * distanceBetweenBlocks)
 
 # Initialize the first cell
 
+
+pass = 0
+
 # Create a double variable to track increasing system time
+time = 0.0
+
+ratio = 0.0
 
 # Check for mass consistency
 
