14b4974e4d899f1aeafca968e76a40a06b33915b
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-29 08:40:09 -0400

    Comments added

--- python/diffusion.py
+++ python/diffusion.py
@@ -5,3 +5,19 @@ School Year: 2019-2020 School Year
 """
 
 maxSize = 10
+
+# Zero the cube
+
+# Create a variable for room dimension to represent 5 meters
+
+# Set a gas molecule speed variable to represent 100 g/mol gas molecules' speed
+# at room temperature
+
+# Create a timestep variable to represent the value of h in seconds
+
+# Initialize the first cell
+
+# Create a double variable to track increasing system time
+
+# Check for mass consistency
+
