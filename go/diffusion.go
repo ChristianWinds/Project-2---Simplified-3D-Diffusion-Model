c64cc3fcddced8c1d37e0782e36345cba8d2238a
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-29 10:27:56 -0400

    Updated

--- go/diffusion.go
+++ go/diffusion.go
@@ -5,11 +5,15 @@
 // Code from Dr. Pounds,
 // /tmp/diffusion.java
 // Accessed Tuesday, October 29th, 2019
+package main
+
+import "fmt"
 
 func main() {
 
 	var maxSize int = 10
 
-
+	fmt.Println("  ")
+	fmt.Println("Box equilibrated in  seconds of simulated time.")
 
 }
