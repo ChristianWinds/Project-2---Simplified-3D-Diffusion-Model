83b9a1006021819e8a51430f7b766b73f0413946
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-10-29 10:18:25 -0400

    Added code citation comment

--- go/diffusion.go
+++ go/diffusion.go
@@ -1,3 +1,8 @@
 // Programmer: Christian Winds
 // Course: CSC 330 Organization of Programming Languages
 // School Year: 2019-2020 School Year
+
+// Code from Dr. Pounds,
+// /tmp/diffusion.java
+// Accessed Tuesday, October 29th, 2019
+
