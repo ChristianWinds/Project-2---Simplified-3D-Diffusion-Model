ecee3fedd5f9ab2215fdfe9e86f9d03710680164
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-09 15:37:42 -0500

    Correction

--- lisp/diffusion.lisp
+++ lisp/diffusion.lisp
@@ -1,7 +1,7 @@
 #|
- | Programmer: Christian Winds
- | Course: CSC 330 Organization of Programming Languages
- | School Year: 2019-2020 School Year
+  Programmer: Christian Winds
+  Course: CSC 330 Organization of Programming Languages
+  School Year: 2019-2020 School Year
 |#
 
 #!/usr/bin/sbcl --script
