ac4a3a83d1da4f2b5c8d3f67c00d1fe2d684f390
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-09 17:06:18 -0500

    Updated

--- r/diffusion.r
+++ r/diffusion.r
@@ -6,6 +6,22 @@
 
 maxsize = 10
 
+diffusion_coefficient = 0.175
+
+# Create a variable for room dimension to represent 5 meters
+room_dimension = 5
+
+# Set a gas molecule speed variable to represent 100 g/mol gas molecules' speed
+# at room temperature
+speed_of_gas_molecules = 250.0
+
+# Create a timestep variable to represent the value of h in seconds
+timestep = (room_dimension / speed_of_gas_molecules) / maxsize
+
+distanceBetweenBlocks = room_dimension / maxsize
+
+DTerm = diffusion_coefficient * timestep / (distance_between_blocks * distance_between_blocks)
+
 time = 0.0
 
 cat ("Box equilibrated in", time, "seconds of simulated time.\n")
