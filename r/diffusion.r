ad12e6bf74cd7b1f773ceeec176d50e3c33ae06e
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-09 16:53:30 -0500

    Updated

--- r/diffusion.r
+++ r/diffusion.r
@@ -5,3 +5,8 @@
 # School Year: 2019-2020 School Year
 
 maxsize = 10
+
+# Code from Stack Overflow,
+# https://stackoverflow.com/questions/15589601/print-string-and-variable-contents-on-the-same-line-in-r
+# Accessed Saturday, November 9th, 2019
+print(paste0("Box equilibrated in ", time, " seconds of simulated time."))
