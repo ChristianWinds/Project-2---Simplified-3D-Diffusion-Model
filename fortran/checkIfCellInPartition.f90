fdfbf40d19513e92c22490bcddca8943a389482c
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-15 02:13:21 -0500

    Module upload

---
+++ fortran/checkIfCellInPartition.f90
@@ -0,0 +1,43 @@
+! Programmer: Christian Winds
+! Course: CSC 330 Organization of Programming Languages
+! School Year: 2019-2020 School Year
+
+module checkIfCellInPartition
+
+! Precondition: All coordinates received by this function are valid
+! three-dimensional coordinates, and the received cell coordinates are valid
+! three-dimensional array cell coordinates. Each X, Y, and Z "Min" partition
+! variable value is also less than or equal to the respective X, Y, and Z "Max"
+! variable value.
+! Postcondition: A Boolean of whether the received array cell coordinates were
+! in the specified partition area was returned to this function's caller.
+
+        implicit none
+        private
+        LOGICAL :: cell_in_partition
+
+        integer :: arrayCellX
+        integer :: arrayCellY
+        integer :: arrayCellZ
+        integer :: partitionXMin
+        integer :: partitionXMax
+        integer :: partitionYMin
+        integer :: partitionYMax
+        integer :: partitionZMin
+        integer :: partitionZMax
+
+        cell_in_partition = .FALSE.
+
+        ! Compare the array cell coordinates and partition cell coordinates to
+        ! determine whether the array cell exists within the partition
+        if (((arrayCellX >= partitionXMin) .and.&
+           &(arrayCellX < partitionXMax)) .and.&
+           &((arrayCellY >= partitionYMin) .and.&
+           &(arrayCellY < partitionYMax)) .and.&
+           &((arrayCellZ >= partitionZMin) .and.&
+           &(arrayCellZ < partitionZMax))) then
+                cell_in_partition = .TRUE.
+        else
+                cell_in_partition = .FALSE.
+        endif
+
